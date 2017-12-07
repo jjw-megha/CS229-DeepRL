5cf66a0cf6af969b9edbaf98df3674bd86e9fa5a
jjw-megha <jjw.megha@gmail.com>
2017-12-07 14:01:49 -0800

    meta controller initial commit

---
+++ meta_controller.py
@@ -0,0 +1,56 @@
+from object_detection import object_detection
+import cv2
+
+class Meta_Controller:
+
+	def __init__(self):
+		self.state = 'start'
+		self.key_collected = False
+		self.masks = self.load_masks()
+
+	def load_masks(self):
+		obj_det = object_detection()
+		img_rgb = cv2.imread('templates/19.png')
+		img_score_section = img_rgb[15:20, 55:95, :]
+		img_game_section = img_rgb[30:,:,:]
+		object_masks = obj_det.detect_objects(img_game_section)
+		masks = {}
+		masks['ladder1'] = object_masks['ladder'][0]
+		masks['ladder2'] = object_masks['ladder'][2]
+		masks['ladder3'] = object_masks['ladder'][1]
+		masks['key'] = object_masks['key'][0]
+		masks['door2'] = object_masks['door'][1]
+		return masks
+
+	def getSubgoal(self):
+		if self.key_collected:
+			return ('door2', self.masks['door2'])
+		if self.state == 'start':
+			return ('ladder1',self.masks['ladder1'])
+		if self.state == 'ladder1':
+			return ('ladder1',self.masks['ladder2'])
+		if self.state == 'ladder2':
+			return ('ladder3',self.masks['ladder3'])
+		if self.state == 'ladder3':
+			return ('key',self.masks['key'])
+		if self.state == 'key':
+			return ('key', self.masks['key'])
+		return ('door2',self.masks['door2'])
+
+	def update_state(self, new_state):
+		self.state = new_state
+
+	def got_key(self):
+		self.key_collected = True
+
+def main():
+	meta = Meta_Controller()
+	subgoal, subgoal_image = meta.getSubgoal()
+	cv2.imshow('image',subgoal_image)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+	main()
+
+
